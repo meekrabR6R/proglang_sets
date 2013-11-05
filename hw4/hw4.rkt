b9e9fc5cd5bd773911f728c8b41e54d8f7ca8730
meekrabR6R <nmiano84@gmail.com>
2013-11-05 13:55:27 -0500

    problem 1, set 4

--- hw4/hw4.rkt
+++ hw4/hw4.rkt
@@ -2,4 +2,13 @@
 
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
-;; put your code below
+;; Problem 1
+(define (sequence low high stride)
+  (define (seq-helper x xs)
+    (if (> x high) 
+        xs
+        (seq-helper (+ x stride) (cons x xs))))
+  (reverse (seq-helper low null)))
+
+;; Problem 2
+(define (string-append-map xs suffix) (list "this" "is" "false"))
